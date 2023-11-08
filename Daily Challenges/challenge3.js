e51f4aceabd9d117c8e5b9c3fee2616548e13f59
Aayush <aayush.paigwar123@gmail.com>
2023-11-08 18:11:00 +0530

    Challenge 3 Completed✅

---
+++ Daily Challenges/challenge3.js
@@ -0,0 +1,15 @@
+/*
+Write a JavaScript program to check whether two given integer values are in the range 50..99 (inclusive). Return true if either of them falls within the range.
+*/
+
+function checkRange(num1, num2) {
+  if ((num1 >= 50 && num1 <= 99) || (num2 >= 50 && num2 <= 99)) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+console.log(checkRange(50, 99));
+console.log(checkRange(49, 100));
+console.log(checkRange(51, 98));
