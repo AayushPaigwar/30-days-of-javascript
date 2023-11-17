7acf5901308b75d96f2974473b7a2ae9b3fafab6
Aayush <aayush.paigwar123@gmail.com>
2023-11-17 16:07:05 +0530

    Challenge 12: Added AM:PM

--- Daily Challenges/challenge12.js
+++ Daily Challenges/challenge12.js
@@ -16,11 +16,12 @@ function clock() {
   let hours = date.getHours();
   let minutes = date.getMinutes();
   let seconds = date.getSeconds();
+  let ampm = hours >= 12 ? "PM" : "AM";
 
   hours = hours < 10 ? "0" + hours : hours;
   minutes = minutes < 10 ? "0" + minutes : minutes;
   seconds = seconds < 10 ? "0" + seconds : seconds;
-  console.log(hours + ":" + minutes + ":" + seconds);
+  console.log(hours + ":" + minutes + ":" + seconds + " " + ampm);
 }
 
 setInterval(clock, 1000);
